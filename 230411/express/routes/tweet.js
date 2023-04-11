ef702bf5a0a95d2e59af5fdbabfb8a8920dc284b
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-11 10:59:34 +0900

    📜유저, 트윗 CRUD

--- 230411/express/routes/tweet.js
+++ 230411/express/routes/tweet.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/:id", (req, res) => {
-  res.send("트윗 조회");
+router.get("/", (req, res) => {
+  res.send("전체 트윗 조회");
 });
 
+router.get("/:id", (req, res) => {
+  console.log(req.params);
+  res.send("특정 트윗 조회");
+});
+
+router.post("/create", (req, res) => {
+  res.send("게시물 생성");
+});
+
+router.put("/update", (req, res) => {
+  res.send("게시물 수정");
+});
+
+router.delete("/delete", (req, res) => {
+  res.send("게시물 삭제");
+});
 module.exports = router;
--- 230411/express/routes/user.js
+++ 230411/express/routes/user.js
@@ -7,4 +7,16 @@ router.get("/:id", (req, res) => {
   res.send("유저 조회");
 });
 
+router.post("/create", (req, res) => {
+  res.send("회원가입");
+});
+
+router.put("/update", (req, res) => {
+  res.send("회원 정보 수정");
+});
+
+router.delete("/delete", (req, res) => {
+  res.send("회원 정보 삭제");
+});
+
 module.exports = router;
