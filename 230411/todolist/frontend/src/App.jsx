04ed153b8fe3c6cf6b2fb53e5a9ccff7818296dd
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-12 11:27:17 +0900

    front-axios 설치 완료!

--- 230411/todolist/frontend/src/App.jsx
+++ 230411/todolist/frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import TodoCard from "./components/TodoCard";
 
 function App() {
--- 230411/todolist/frontend/src/components/TodoCard.jsx
+++ 230411/todolist/frontend/src/components/TodoCard.jsx
@@ -3,10 +3,14 @@ import { useState } from "react";
 const TodoCard = ({ title }) => {
   const [isDone, setIsDone] = useState(false);
 
+  const onClickToggle = () => {
+    setIsDone(!isDone);
+  };
+
   return (
     <>
       {isDone ? (
-        <li className="flex my-4">
+        <li className="flex my-4" onClick={onClickToggle}>
           <div className="relative">
             <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-4 border-white bg-pink-400 w-8 h-8 scale-75 rounded-xl"></div>
@@ -14,7 +18,7 @@ const TodoCard = ({ title }) => {
           <div className="text-2xl ml-4">{title}</div>
         </li>
       ) : (
-        <li className="flex my-4">
+        <li className="flex my-4" onClick={onClickToggle}>
           <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
           <div className="text-2xl ml-4">{title}</div>
           {/* 완료처리되면 line-through */}
